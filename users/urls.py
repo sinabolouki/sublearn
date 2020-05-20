635a26554fcb19086fd5d7bd094c2476d8b63041
SinaBolouki <sina.bolouki@gmail.com>
2020-05-20 23:18:37 +0430

    send signup code added to urls

--- users/urls.py
+++ users/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path(r'logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path(r'confirm/', auth_views.LogoutView.as_view(template_name='users/confirm.html'), name="confirm"),
     path(r'index/', views.user_index, name='index'),
+    path(r'send_signup_code/', views.send_code, name='send_signup_code')
 ]
