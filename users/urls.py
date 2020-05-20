36c2b42469505b9de570cff7f1a52401ad332de5
Unknown <unknown@example.com>
2020-05-20 22:10:28 +0430

    confirm page added

--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path(r'profile/', views.user_profile, name="profile"),
     path(r'login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path(r'logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path(r'confirm/', auth_views.LogoutView.as_view(template_name='users/confirm.html'), name="confirm"),
     path(r'index/', views.user_index, name='index'),
 ]
