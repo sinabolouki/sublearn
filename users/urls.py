9d0a0b8b0f2825495676ad525762a8a5e21906af
sepehr <sepehr.feli.1377@gmail.com>
2020-05-22 22:34:23 +0430

    Forgot Password page created

--- users/urls.py
+++ users/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path(r'login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path(r'logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path(r'confirm/', auth_views.LogoutView.as_view(template_name='users/confirm.html'), name="confirm"),
+    path(r'forgotPassword/', auth_views.LogoutView.as_view(template_name='users/forgotPassword.html'), name="forgotPassword"),
     path(r'index/', views.user_index, name='index'),
     path(r'send_signup_code/', views.send_code, name='send_signup_code')
 ]
