482521393a248a0c0df89747116380a77411fa8b
SinaBolouki <sina.bolouki@gmail.com>
2020-06-15 18:32:20 +0430

    Auto stash before merge of "master" and "origin/master"

--- subtitles/urls.py
+++ subtitles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from subtitles import views
+from django.views.generic import TemplateView
 
 app_name="subtitles"
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', views.sub_processor, name='sub_processor'),
     path('download_sub', views.download_sub, name='download_sub'),
     path('download_words_list', views.download_words_list, name='download_words_list'),
+    path('download/', TemplateView.as_view(template_name='subtitles/download.html'), name='download_outside_sub')
 ]
