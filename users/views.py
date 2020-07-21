a898a1c494be4cfe34767d77308f34da07e909a0
SinaBolouki <sina.bolouki@gmail.com>
2020-07-21 22:34:09 +0430

    some changes on premium date

--- users/views.py
+++ users/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.mail import send_mail, EmailMessage
